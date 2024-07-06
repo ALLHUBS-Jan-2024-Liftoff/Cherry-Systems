fa03bf6c3236ccc2a4fd51074ec5b91607351ac8
chrimcha <147195321+chrimcha@users.noreply.github.com>
2024-07-06 13:24:06 -0500

    added gitignore, setup applications for UI and Backend, added script for bootstrap in UI, and updated application properties for Backend

---
+++ ThirdPlace-Backend/src/main/java/com/CherrySystems/ThirdPlace_Backend/ThirdPlaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.CherrySystems.ThirdPlace_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPlaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdPlaceBackendApplication.class, args);
+	}
+
+}
