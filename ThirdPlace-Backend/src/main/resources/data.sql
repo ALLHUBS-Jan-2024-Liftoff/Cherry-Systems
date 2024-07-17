33af4dc93671e4356eff9b4463557661394fdb9a
austinrakey <rakeysound@gmail.com>
2024-07-17 01:49:26 -0500

    starter schema, starter seed data, fixed an error in application.properties

---
+++ ThirdPlace-Backend/src/main/resources/data.sql
@@ -0,0 +1,33 @@
+-- sample users
+INSERT INTO Users (username, pwHash, email) VALUES
+('user1', 'hashed_password_1', 'user1@example.com'),
+('user2', 'hashed_password_2', 'user2@example.com'),
+('user3', 'hashed_password_3', 'user3@example.com');
+
+-- sample submissions
+INSERT INTO Submissions (user_id, location_name, location_address, Place_id, rating, description, submission_review)
+VALUES
+(1, 'Mokabes Coffee House', '3606 Arsenal St, St. Louis, MO 63116, USA', 'ChIJEU0-nWe02IcR5Idj1tWs-To', 5, 'Quiet coffee house.', 'Great coffee!'),
+(2, 'City Museum', '750 N 16th St, St. Louis, MO 63103, USA', 'ChIJVR6Awj2z2IcRBMqKMCBEfMk', 4, 'Giant art park where you can climb everything', 'Art is cool!'),
+(3, 'Forest Park', '5595 Grand Dr, St. Louis, MO 63112, USA', 'ChIJycR4kUa12IcRqpSybQLFJ50', 2, 'Very big park, with museums ', 'Not as cool as Tower Grove Park!');
+
+-- sample reviews
+INSERT INTO Reviews (submission_id, user_id, rating, review)
+VALUES
+(1, 2, 5, 'Amazing experience, loved the scenery!'),
+(2, 3, 4, 'Very crowded, but still worth visiting.'),
+(3, 1, 1, 'Worst Park I have ever been to!');
+
+-- sample review votes
+INSERT INTO ReviewVotes (review_id, user_id, vote_type)
+VALUES
+(1, 1, 'up'),
+(2, 3, 'down'),
+(3, 2, 'up');
+
+-- sample submission votes
+INSERT INTO SubmissionVotes (submission_id, user_id, vote_type)
+VALUES
+(1, 1, 'up'),
+(2, 3, 'down'),
+(3, 2, 'down');
