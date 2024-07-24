ae09a563295eb02323774bc13a9d7d899914f21b
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-07-24 13:59:12 -0500

    Call PlacesService and return PlaceResult object with custom fields

--- ThirdPlace-UI/src/PlaceIdMap.jsx
+++ ThirdPlace-UI/src/PlaceIdMap.jsx
@@ -15,6 +15,7 @@ export default function MapSample() {
     const {LatLng} = await google.maps.importLibrary("core");
     const { Place } = await google.maps.importLibrary("places");
 
+
     // const coords = new google.maps.places.PlaceDetailsRequest
     // const place = PlacesService.getDetails();
 
@@ -31,7 +32,7 @@ export default function MapSample() {
 
 //geocode placeid 
 
-const geocoder = new google.maps.Geocoder(); 
+// const geocoder = new google.maps.Geocoder(); 
 
 // function codeAddress() {
 //   const placeId = "ChIJh25wB-7_dYgR_PxH5m5ScMA";
@@ -72,57 +73,51 @@ const geocoder = new google.maps.Geocoder();
 //   });
 
 
+//render map; this breaks when moved within callback function
 
+map = new Map(document.getElementById("map"), {
+  center:  new google.maps.LatLng(38.90595959069724, -90.1118910957208),
+  // center:  new google.maps.LatLng(38.90595959069724, -90.1118910957208),
+  zoom: 16,
+  mapId: 'DEMO_MAP_ID',
+  disableDefaultUI: false,
+  mapTypeControl: false,
+  streetViewControl: false,
+  rotateControl: false,
+});
 
+const marker = new AdvancedMarkerElement({
+  map,
+  position: { lat: 38.60, lng: -90.24 },
+});
 
-    map = new Map(document.getElementById("map"), {
-      center:  new google.maps.LatLng(38.90595959069724, -90.1118910957208),
-    //   center:  new google.maps.LatLng({testCoords}),
-      zoom: 16,
-      mapId: 'DEMO_MAP_ID',
-      disableDefaultUI: false,
-      mapTypeControl: false,
-      streetViewControl: false,
-      rotateControl: false,
-    });
-
-    const marker = new AdvancedMarkerElement({
-      map,
-      position: { lat: 38.60, lng: -90.24 },
-    });
+const callbackTestCoords = {};
 
 
     //TODO grab place details, pick video back up at 3:11
     // https://www.youtube.com/watch?v=vAK5o8h8C28
+
+    // Calls PlacesService via the PlaceDetailsRequest interface, and returns a PlaceResult object with field options
     const request = {
       placeId: 'ChIJh25wB-7_dYgR_PxH5m5ScMA',
-      fields: ['name', 'opening_hours']
+      fields: ['name', 'opening_hours', 'geometry']
     };
     const service = new google.maps.places.PlacesService(map);
     service.getDetails(request, callback);
+ 
+    function callback(place, status) {
+      // if (status !== google.maps.places.PlacesServiceStatus.OK) return;
+      console.log(`${place.name} is open at ${place.opening_hours.weekday_text[0]} and located at ${place.geometry.location}`);
+
+      const callbackTestCoords = place.geometry.location;
+      console.log(`printing callbackTestCoords: ${callbackTestCoords}`)
+      console.log(`typeof callbackTestCoords: ${typeof(callbackTestCoords)}`)
+    }
+
+    console.log(`printing callbackTestCoords outside function: ${callbackTestCoords}`)
+
 
   }
-
-  // Pull formatted address from a PlaceId
-
-  // async function getPlaceDetails(Place) {
-
-  //   //use place details
-  //   const place = new Place({
-  //     id: placeIdString,
-  //     requestedLanguage: "en", // optional
-  //   });
-    
-  //   // Call fetchFields, passing the desired data fields.
-  //   await place.fetchFields({
-  //     fields: ["displayName", "formattedAddress", "location"],
-  //   });
-  //   // Log the result
-  //   console.log(place.displayName);
-  //   console.log(place.formattedAddress);
-  //   console.log(place.location);
-
-  //   };
 
 
   initMap();
