5886bc714a4949dba0f09604fb4bcba7bd655375
austinrakey <rakeysound@gmail.com>
2024-07-25 00:58:13 -0500

    schema and data initialization working, need to manually switch init setting

--- ThirdPlace-Backend/src/main/resources/schema.sql
+++ ThirdPlace-Backend/src/main/resources/schema.sql
@@ -1,16 +1,26 @@
-CREATE TABLE debug_table (
+-- These commented out commands are for if you want to reset your db to a clean slate between restarts.
+
+DROP TABLE IF EXISTS debug_table;
+DROP TABLE IF EXISTS favorite;
+DROP TABLE IF EXISTS review_vote;
+DROP TABLE IF EXISTS submission_vote;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS submission;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users_seq;
+
+CREATE TABLE IF NOT EXISTS debug_table (
     id INT AUTO_INCREMENT PRIMARY KEY
 );
 
-
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     pw_Hash VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
 );
 
-CREATE TABLE submission (
+CREATE TABLE IF NOT EXISTS submission (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     location_name VARCHAR(255) NOT NULL,
@@ -23,7 +33,7 @@ CREATE TABLE submission (
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
-CREATE TABLE review (
+CREATE TABLE IF NOT EXISTS review (
     id INT AUTO_INCREMENT PRIMARY KEY,
     submission_id INT NOT NULL,
     user_id INT NOT NULL,
@@ -34,7 +44,7 @@ CREATE TABLE review (
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
-CREATE TABLE review_vote (
+CREATE TABLE IF NOT EXISTS review_vote (
     id INT AUTO_INCREMENT PRIMARY KEY,
     review_id INT NOT NULL,
     user_id INT NOT NULL,
@@ -43,7 +53,7 @@ CREATE TABLE review_vote (
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
-CREATE TABLE submission_vote (
+CREATE TABLE IF NOT EXISTS submission_vote (
     id INT AUTO_INCREMENT PRIMARY KEY,
     submission_id INT NOT NULL,
     user_id INT NOT NULL,
@@ -52,7 +62,7 @@ CREATE TABLE submission_vote (
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
-CREATE TABLE favorite (
+CREATE TABLE IF NOT EXISTS favorite (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     submission_id INT NOT NULL,
@@ -60,12 +70,24 @@ CREATE TABLE favorite (
     FOREIGN KEY (submission_id) REFERENCES Submission(id)
 );
 
+--ALTER TABLE review_vote
+--DROP INDEX unique_user_review_vote;
+
 ALTER TABLE review_vote
 ADD CONSTRAINT unique_user_review_vote UNIQUE (user_id, review_id);
+
+
+--ALTER TABLE submission_vote
+--DROP INDEX unique_user_submission_vote;
 
 ALTER TABLE submission_vote
 ADD CONSTRAINT unique_user_submission_vote UNIQUE (user_id, submission_id);
 
+
+--ALTER TABLE favorite
+--DROP INDEX unique_user_submission_favorite;
+
 ALTER TABLE favorite
 ADD CONSTRAINT unique_user_submission_favorite UNIQUE (user_id, submission_id);
+
 
