5c1d0f5853ea939b4852653d95bd2c5f5b0e75ac
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-07-28 21:39:11 -0500

    Initial commit, read-only search bar added

--- ThirdPlace-UI/src/components/pages/SearchAndList.jsx
+++ ThirdPlace-UI/src/components/pages/SearchAndList.jsx
@@ -4,7 +4,6 @@ import Navbar from "../navigation/Navbar";
 import CondensedSubmission from "../condensed-submission/CondensedSubmission.jsx";
 
 export default function SearchAndList() {
-  
   const [submissions, setSubmissions] = useState([]);
 
   useEffect(() => {
@@ -23,6 +22,47 @@ export default function SearchAndList() {
       <Navbar />
 
       <h1>View Locations</h1>
+
+      <form method="post">
+        <div className="form-group">
+          <h5>Search by:</h5>
+
+          <div className="form-check form-check-inline">
+
+          <input className="form-check-input" type="radio" name="searchAll" value="all"/>
+          <label className="form-check-label">
+            <span> All</span> 
+          </label>
+          </div>
+
+          <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="searchByName" value="name" disabled/>
+          <label className="form-check-label">
+            <span> Name</span> 
+          </label>
+          </div>
+
+          <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="searchByAddress" value="address" disabled/>
+          <label className="form-check-label">
+            <span> Address</span> 
+          </label>
+          </div>
+
+          <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="searchByRating" value="rating" disabled/>
+          <label className="form-check-label">
+            <span> Rating</span> 
+            </label>
+            </div>
+
+          <input className="form-control" name="searchQuery" />
+          
+        </div>
+
+        <input type="submit" className="submit-button" value="Search" />
+      </form>
+
       <table className="table table-striped border shadow">
         <tbody>
           {submissions.map((submission) => (
