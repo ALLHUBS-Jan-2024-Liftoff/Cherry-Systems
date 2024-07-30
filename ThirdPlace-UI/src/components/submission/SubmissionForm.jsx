169406bfaa84fb8fb5e06eb9aa3328b85ef4d5c1
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-07-29 19:43:29 -0500

    RateAndReview Component complete

--- ThirdPlace-UI/src/components/submission/SubmissionForm.jsx
+++ ThirdPlace-UI/src/components/submission/SubmissionForm.jsx
@@ -21,13 +21,9 @@ export default function SubmissionForm() {
                         <textarea name="description" rows="4"/>
                     </label>
                 </div>
+                <br></br>
                 <div>
-                    <RateAndReview rating={4} />
-                </div>
-                <div className="form-group">
-                    <label>Review <br></br>
-                        <textarea name="submissionReview" rows="4" />
-                    </label>
+                    <RateAndReview />
                 </div>
 
                 <input type="submit" className="submit-button" value="Submit Location" />
