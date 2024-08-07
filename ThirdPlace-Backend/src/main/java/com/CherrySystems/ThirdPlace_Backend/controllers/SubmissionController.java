7f9729897f948dd9b4755524cef5b18c70487eda
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-08-06 21:30:30 -0500

    Submission controller now successfully saves new submission and prevents duplication, viewByID, and deletes entries by ID

--- ThirdPlace-Backend/src/main/java/com/CherrySystems/ThirdPlace_Backend/controllers/SubmissionController.java
+++ ThirdPlace-Backend/src/main/java/com/CherrySystems/ThirdPlace_Backend/controllers/SubmissionController.java
@@ -1,8 +1,11 @@
 package com.CherrySystems.ThirdPlace_Backend.controllers;
 
 import com.CherrySystems.ThirdPlace_Backend.models.Submission;
+import com.CherrySystems.ThirdPlace_Backend.models.User;
 import com.CherrySystems.ThirdPlace_Backend.models.dto.SubmissionFormDTO;
 import com.CherrySystems.ThirdPlace_Backend.repositories.SubmissionRepository;
+import com.CherrySystems.ThirdPlace_Backend.repositories.UserRepository;
+import jakarta.persistence.Id;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,74 +24,55 @@ public class SubmissionController {
     @Autowired
     private SubmissionRepository submissionRepository;
 
+    @Autowired
+    private UserRepository userRepository;
 
-    //Processes Submission Form and displays new submission listing
-    // { "locationName":"Kaldi Coffee", "locationAddress":"Somewhere in STL", "rating":"4", "description":"Coffee Shop", "submissionReview":"This place has great coffee" }
+
+//Processes Submission Form - Takes form submission data in JSON form to create an object.
+//Postman request I was using for testing --> { "locationName":"Kaldi Coffee", "locationAddress":"Somewhere in STL", "rating":"4", "description":"Coffee Shop", "submissionReview":"This place has great coffee" }
     @PostMapping("/form")
     public ResponseEntity<?> newSubmission(@RequestBody SubmissionFormDTO submissionFormDTO) {
+
         Submission newSubmission = new Submission();
+
         newSubmission.setLocationName(submissionFormDTO.getLocationName());
         newSubmission.setLocationAddress(submissionFormDTO.getLocationAddress());
         newSubmission.setRating(submissionFormDTO.getRating());
         newSubmission.setDescription(submissionFormDTO.getDescription());
         newSubmission.setSubmissionReview(submissionFormDTO.getSubmissionReview());
-        submissionRepository.save(newSubmission);
-        return ResponseEntity.ok("New Submission completed");
 
+//TODO: get user from session/authentication, currently holding dummy data to appease constructor
+        User user1 = userRepository.findByUsername("user1");
+        newSubmission.setUser(user1);
+
+//TODO: get placeID from Maps API address, currently holding dummy data to appease constructor
+        newSubmission.setPlaceId("123abc");
+
+        String currentLocationName = submissionFormDTO.getLocationName();
+        Submission isLocationInRepository = submissionRepository.findByLocationName(currentLocationName);
+
+
+//Looks in Submission Repository for submissions with the same location. Will only save newSubmission if database search returns null
+        if (isLocationInRepository == null) {
+            submissionRepository.save(newSubmission);
+            return ResponseEntity.ok("New Submission completed");
+        } else {
+            return ResponseEntity.badRequest().body("Location has already been submitted.");
+        }
     }
-//    public Submission newSubmission(String locationName, String locationAddress, Integer rating, String description, String submissionReview) {
-//        Submission newSubmission = new Submission();
-//        newSubmission.setLocationName(locationName);
-//        newSubmission.setLocationAddress(locationAddress);
-//        newSubmission.setRating(rating);
-//        newSubmission.setDescription(description);
-//        newSubmission.setSubmissionReview(submissionReview);
-//        submissionRepository.save(newSubmission);
-//        return newSubmission;
-//
-//    }
-
-//    public ResponseEntity<?> addSubmission(@RequestBody SubmissionFormDTO submissionFormDTO, HttpServletRequest request) {
-//
-//        String locationName = submissionFormDTO.getLocationName();
-//        String locationAddress = submissionFormDTO.getLocationAddress();
-//        Integer rating = submissionFormDTO.getRating();
-//        String description = submissionFormDTO.getDescription();
-//        String submissionReview = submissionFormDTO.getSubmissionReview();
-//
-//
-//    }
-
-//
-//        if (errors.hasErrors()) {
-//            return ResponseEntity.badRequest().body(errors.getAllErrors());
-//        }
-//
-//
-
-//        Submission currentSubmission = submissionRepository.findByLocationName(locationName);
-
-//        addSubmission()
-//            submissionRepository.save(addSubmission);
-//        }
-
-//        return ResponseEntity.ok("New Submission completed");
-//    }
 
 
 
-
-
-    //    View all Submissions
+//View all Submissions
     @GetMapping("/searchandlist")
     public List<Submission> getAllSubmissions(){
         return (List<Submission>) submissionRepository.findAll();
     }
 
 
-//    View Each Submission by ID
-//    TODO: Could create a CondensedSubmissionView model to show/view only locationName, locationAddress, description, rating, submissionReview.
-//    TODO: Submission model to inherit CondensedSubmissionView
+//View Each Submission by ID
+//TODO: Could create a CondensedSubmissionView model to show/view only locationName, locationAddress, description, rating, submissionReview.
+//TODO: Submission model to inherit CondensedSubmissionView
     @GetMapping("/{id}")
     public ResponseEntity<?> viewSubmissionById(@PathVariable Integer id) {
         Optional<Submission> submissionById = submissionRepository.findById(id);
@@ -100,5 +84,9 @@ public class SubmissionController {
         }
     }
 
-
+//Deletes submissions in Submission Repository by finding the submission by its ID#
+    @DeleteMapping("/{id}")
+    public void deleteSubmission(@PathVariable Integer id) {
+        submissionRepository.deleteById(id);
+    }
 }
