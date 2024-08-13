71c840ffae7796781526e20bcb62fea0048cfeb1
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-08-13 13:39:17 -0500

    add categorymenu component and css file, dropdown populates categories

--- ThirdPlace-UI/src/components/submission/CategoryMenu.jsx
+++ ThirdPlace-UI/src/components/submission/CategoryMenu.jsx
@@ -1,23 +1,35 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import './CategoryMenu.css';
 
-export const CategoryMenu = () => {
-    const [categoriesList, setCategoriesList] = useState("");
+export function CategoryMenu() {
+    const [categoriesList, setCategoriesList] = useState({});
+    const [isDropdownDisplayed, setIsDropDownDisplayed] = useState(false);
+    const [selectedCategories, setSelectedCategories] = useState(
+        // categoriesList.reduce((obj, category) => ({...obj, [category.id]: false}), []) 
+    );
+
+    console.log('selectedCategories', selectedCategories);
 
     const loadCategories = async () => {
         try {
             const response = await axios.get('http://localhost:8080/api/category/all');
- 
+
             const data = response.data;
             const allCategoriesArr = [];
 
-            for(let i in data) {
-                allCategoriesArr.push(data [i]);
+            for (let i in data) {
+                allCategoriesArr.push(data[i]);             
             }
 
+            const keyValuePairCategories = allCategoriesArr.reduce((obj, category) => (
+                {...obj, [ category.id ]: false}
+            ), {});
+
+            setSelectedCategories(keyValuePairCategories);
+
             console.log(allCategoriesArr);
-            setCategoriesList(allCategoriesArr);
-            return categoriesList;
+            return allCategoriesArr;
         } catch (error) {
             console.error("Error loading categories list from database!");
             throw error;
@@ -25,44 +37,36 @@ export const CategoryMenu = () => {
     }
 
     useEffect(() => {
-        loadCategories();
+        loadCategories().then(setCategoriesList);
     }, []);
 
-    const categoryOptions = () => {
-        categoriesList.map((category) => ( <p>{category}</p>
-        // <div>
-        //     {category.name}
-        // </div>
-    ))}
-
     return (
-        <div className='dropdown'>
-            <label className='dropdown-label'>Select Categories</label>
-
-            <div className='dropdown-list'>
-                <label className='dropdown-option'>
-                    <input type='checkbox' name='dropdown-group' value={1}/>
-                    Category with id: 1
-                </label>
-            </div>
-            <div className='dropdown-list'>
-                <label className='dropdown-option'>
-                    <input type='checkbox' name='dropdown-group' value={2}/>
-                    Category with id: 2
-                </label>
-            </div>
-            <div className='dropdown-list'>
-                <label className='dropdown-option'>
-                    <input type='checkbox' name='dropdown-group' value={3}/>
-                    Category with id: 3
-                </label>
-            </div>
-            <div className='dropdown-list'>
-                <label className='dropdown-option'>
-                    <input type='checkbox' name='dropdown-group' value={4}/>
-                    Category with id: 4
-                </label>
-            </div>
-        </div>
+        <>
+            <fieldset className='category-dropdown-div'>
+                <div 
+                    className='category-dropdown'
+                    onClick={() => setIsDropDownDisplayed((prevState) => !prevState)}>
+                    ---select categories---
+                </div>
+                {isDropdownDisplayed && <div className='panel'>
+                    {categoriesList.map((category) => (
+                        <fieldset key={category.id}>
+                            <input
+                                onChange={(e) => setSelectedCategories({
+                                    [category.id]: e.target.checked,
+                                })}
+                                checked={selectedCategories[category.id]}
+                                id={`input-${category.id}`} type='checkbox'/>
+                            <label 
+                                htmlFor={`input-${category.id}`}
+                                className='category-dropdown-option'
+                            >
+                                    {category.name}
+                            </label>
+                        </fieldset>
+                    ))}
+                </div>}
+            </fieldset>
+        </>
     )
 }
