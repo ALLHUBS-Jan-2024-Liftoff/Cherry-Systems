7840ee57f562e0210de0376c6c8ce33469085ad5
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-08-13 17:15:11 -0500

    added more css to categories dropdown, can click out of dropdown if click outside of dropdown panel or click on select bar

--- ThirdPlace-UI/src/components/submission/CategoryMenu.jsx
+++ ThirdPlace-UI/src/components/submission/CategoryMenu.jsx
@@ -1,15 +1,31 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './CategoryMenu.css';
 
 export function CategoryMenu() {
     const [categoriesList, setCategoriesList] = useState({});
     const [isDropdownDisplayed, setIsDropDownDisplayed] = useState(false);
-    const [selectedCategories, setSelectedCategories] = useState(
-        // categoriesList.reduce((obj, category) => ({...obj, [category.id]: false}), []) 
-    );
+    const [selectedCategories, setSelectedCategories] = useState({});
 
-    console.log('selectedCategories', selectedCategories);
+    const numberOfCategoriesSelected = Object.values(selectedCategories).filter(Boolean).length
+
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        const onClick = (e) => {
+            if(e.target !== dropdownRef.current) {
+                console.log("here");
+                setIsDropDownDisplayed(false);
+            }
+        };
+
+        document.addEventListener('click', onClick);
+
+        return () => {
+            document.removeEventListener('click', onClick);
+        };
+
+    }, []);
 
     const loadCategories = async () => {
         try {
@@ -42,30 +58,76 @@ export function CategoryMenu() {
 
     return (
         <>
-            <fieldset className='category-dropdown-div'>
+            <fieldset className='category-dropdown'>
                 <div 
-                    className='category-dropdown'
-                    onClick={() => setIsDropDownDisplayed((prevState) => !prevState)}>
-                    ---select categories---
+                    className='category-dropdown-div'
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setIsDropDownDisplayed((prevState) => !prevState);
+                    }}
+                >
+                    {numberOfCategoriesSelected > 0 ? `${numberOfCategoriesSelected}  categories selected` : '---select categories---'}
+
+                    {/* caret icons from heroicons.com */}
+                    {!isDropdownDisplayed  ? (
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" 
+                            viewBox="0 0 24 24" 
+                            stroke-width="1.5" 
+                            stroke="currentColor"
+                        >
+                            <path 
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="m19.5 8.25-7.5 7.5-7.5-7.5" 
+                            />
+                        </svg>
+                    ) : (
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" 
+                            viewBox="0 0 24 24" 
+                            stroke-width="1.5" 
+                            stroke="currentColor" 
+                        >
+                            <path 
+                                stroke-linecap="round" 
+                                stroke-linejoin="round" 
+                                d="m4.5 15.75 7.5-7.5 7.5 7.5" 
+                            />
+                        </svg>
+                    )}
+
                 </div>
-                {isDropdownDisplayed && <div className='panel'>
-                    {categoriesList.map((category) => (
-                        <fieldset key={category.id}>
-                            <input
-                                onChange={(e) => setSelectedCategories({
-                                    [category.id]: e.target.checked,
-                                })}
-                                checked={selectedCategories[category.id]}
-                                id={`input-${category.id}`} type='checkbox'/>
-                            <label 
-                                htmlFor={`input-${category.id}`}
-                                className='category-dropdown-option'
+                {isDropdownDisplayed && 
+                    <div
+                        onClick={(e) => e.stopPropagation()}
+                        ref={dropdownRef}
+                        className='panel'
+                    >
+                        {categoriesList.map((category) => (
+                            <fieldset 
+                                key={category.id}
+                                className={selectedCategories[category.id] ? 'selected' : ''}
                             >
-                                    {category.name}
-                            </label>
-                        </fieldset>
-                    ))}
-                </div>}
+                                <input
+                                    onChange={(e) => setSelectedCategories({
+                                        ...selectedCategories,
+                                        [category.id]: e.target.checked,
+                                    })}
+                                    checked={selectedCategories[category.id]}
+                                    id={`input-${category.id}`} type='checkbox'/>
+                                <label 
+                                    htmlFor={`input-${category.id}`}
+                                    className='category-dropdown-option'
+                                >
+                                        {category.name}
+                                </label>
+                            </fieldset>
+                        ))}
+                    </div>
+                }
             </fieldset>
         </>
     )
