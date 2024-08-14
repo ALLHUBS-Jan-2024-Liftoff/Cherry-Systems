b53b9d5815277e130efefe3b04fe455d0a644066
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-08-14 01:54:57 -0500

    at a good point to stop for profileinfocard css, will change with feedback

--- ThirdPlace-UI/src/components/user/CherryScoreBadge.jsx
+++ ThirdPlace-UI/src/components/user/CherryScoreBadge.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './ProfileInfoCard.css';
+import cherryNoLeaf from '../../assets/cherries-no-leaf.png'
+import { useAuth } from '../../context/AuthContext';
 
 export const CherryScoreBadge = () => {
+    const { user } = useAuth();
+
     return (
-        <div>CherryScoreBadge</div>
+        <div className='for-padding'>
+           {user.cherryPoints > 0 ? ( 
+            <div className='cherry-img-and-score'>
+            <p className='cherry-score-title'>
+                Cherry Score:
+            </p>
+            <p className='cherryPoints-and-img'>
+                {user.cherryPoints}
+                <img src={cherryNoLeaf}></img>
+            </p>
+            </div>
+            ) : ("No Cherry Points Yet!")}
+        </div>
     )
 }
