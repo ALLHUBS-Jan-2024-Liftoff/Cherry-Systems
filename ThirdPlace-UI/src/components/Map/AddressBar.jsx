6cef8a5cfc61522193f49e3e3df5a8b5cc6d4fa6
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-08-14 22:19:24 -0500

    Google autocomplete suggestions functional

--- ThirdPlace-UI/src/components/Map/AddressBar.jsx
+++ ThirdPlace-UI/src/components/Map/AddressBar.jsx
@@ -1,19 +1,63 @@
-import.meta.env.GOOGLE_MAPS_API_KEY;
+import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+import React, {useState} from 'react';
+
 
 export default function AddressBar() {
-  const handleSubmit = (event) => {
-    
-  }
+
+  let autocomplete;
+
+  //TODO investigate number of API calls 
+  
+async function initAutoComplete() {
+
+    const {Autocomplete} = await google.maps.importLibrary("places");
+
+    autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById("autocomplete"),
+      {
+        fields: ["name", "place_id"]
+      }
+    );
+    //trigger place selection handler
+    // autocomplete.addListener("place_changed", onPlaceChanged);
+}
+
+
+const [address, setAddress] = useState("");
+const [placeId, setPlaceId] = useState("");
+
+//TODO write handler for place selection checking if a valid selection is made, and if not, grab placeId
+// function onPlaceChanged() {
+//   const place = autocomplete.getPlace();
+
+//   if (!place.geometry) {
+//     document.getElementById("autocomplete").placeholder = "Enter valid address...";
+//   } else {
+//     document.getElementById("details")
+//   }
+// }
+
+
+
+  // const handleSubmit = (event) => {
+    // event.preventDefault();
+  // }
+
+  initAutoComplete();
 
 
   return (
     <>
       <div className="review-card">
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="form-group">
             <label>
               Address <br></br>
-              <input name="locationAddress" />
+              <input name="locationAddress" 
+              id="autocomplete"
+              placeholder="Enter valid address..."
+              value={address}
+              onChange= {(e) => setAddress(e.target.value)}/>
             </label>
           </div>
           <input
