5651c4779deb15dcea1a3da8fcfa9a2a50672f7a
austinrakey <rakeysound@gmail.com>
2024-08-15 18:57:06 -0500

    controller and repository for favorites

---
+++ ThirdPlace-Backend/src/main/java/com/CherrySystems/ThirdPlace_Backend/controllers/FavoriteController.java
@@ -0,0 +1,2 @@
+package com.CherrySystems.ThirdPlace_Backend.controllers;public class FavoriteController {
+}
---
+++ ThirdPlace-Backend/src/main/java/com/CherrySystems/ThirdPlace_Backend/repositories/FavoriteRepository.java
@@ -0,0 +1,16 @@
+package com.CherrySystems.ThirdPlace_Backend.repositories;
+
+import com.CherrySystems.ThirdPlace_Backend.models.Favorite;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FavoriteRepository extends CrudRepository<Favorite, Integer> {
+
+
+    List<Favorite> findByUserId(Integer userId);
+
+    List<Favorite> findBySubmissionId(Integer submissionId);
+}
