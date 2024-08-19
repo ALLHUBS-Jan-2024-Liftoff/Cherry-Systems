1fb622bd5fc9e3c8bba57dc912ecc80a269ef473
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-08-18 22:34:30 -0500

    Time is now hours and minutes

--- ThirdPlace-UI/src/components/condensed-submission/DateTimeStamp.jsx
+++ ThirdPlace-UI/src/components/condensed-submission/DateTimeStamp.jsx
@@ -16,7 +16,7 @@ export default function RenderDateAndTime(submission) {
     const formattedTime = date.toLocaleTimeString('en-US', {
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit'
+        // second: '2-digit'
     });
 
     return ("Submitted on: " + formattedDate + " at " + formattedTime);
