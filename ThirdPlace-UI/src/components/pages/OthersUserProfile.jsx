7c8437ff1cf82884a429964c935d87be338f90d9
chrimcha <147195321+chrimcha@users.noreply.github.com>
2024-08-22 11:28:20 -0500

    changed links css so that links are not blue and underline except for login and registration

--- ThirdPlace-UI/src/components/pages/OthersUserProfile.jsx
+++ ThirdPlace-UI/src/components/pages/OthersUserProfile.jsx
@@ -83,11 +83,11 @@ export default function OthersUserProfile() {
     <div>
         <Navbar/>
           {user === null ? (
-            <section className='review-card'>
+            <section className='review-card-no-user-profile-submit-location'>
                 <h1>Log in to see Profile page!</h1>
                 <br />
                 <p>
-                    <Link to={{ pathname: '/login', state: { user, isAuthenticated }}}>Go to Login</Link>
+                    <Link className="link-css-login-register" to={{ pathname: '/login', state: { user, isAuthenticated }}}>Go to Login</Link>
                 </p>
             </section>
           ) : (
